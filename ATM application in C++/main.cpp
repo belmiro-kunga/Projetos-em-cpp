492a0c824202618b18fea48476e7ecc15be15164
belmiro-kunga <belmirokunga@gmail.com>
2021-09-09 22:44:05 +0100

    Conclusão do projeto
    Projeto para melhorar a logica dos alunos

--- ATM application in C++/main.cpp
+++ ATM application in C++/main.cpp
@@ -1,18 +1,54 @@
-#include <iostream>
+// Aplicação de um ATM em  C++.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
 
+#include <iostream>
 using namespace std;
 
-void showMenu(){
-    cout << "**********MENU**********"<<endl;
-    cout << "1. verificar saldo"<< endl;
-    cout << "2. deposito"<< endl;
-    cout << "3. levantamento"<< endl;
-    cout << "************************"<<endl;
+void showMenu()
+{
+    cout << "**********MENU**********" << endl;
+    cout << "1. verificar saldo" << endl;
+    cout << "2. deposito" << endl;
+    cout << "3. levantamento" << endl;
+    cout << ". Fechar" << endl;
+    cout << "************************" << endl;
 }
 
-int main(){
+int main()
+{
     //verificar saldo, deposito, levantamento,mostrar menu
-    
-    showMenu();
+    int option;
+    double Saldo = 250;
+
+    do
+    {
+        showMenu();
+        cout << "Option: ";
+        cin >> option;
+        system("cls");
+        switch (option)
+        {
+        case 1:
+            cout << "O Saldo da Conta e de " << Saldo << "kz " << endl;
+            break;
+        case 2:
+            cout << "Deposito: ";
+            double deposito;
+            cin >> deposito;
+            Saldo += deposito;
+            break;
+        case 3:
+            cout << "Levantamento: ";
+            double Levantamento;
+            cin >> Levantamento;
+            if (Levantamento <= Saldo)
+                Saldo -= Levantamento;
+            else
+                cout << "Nao tem saldo suficiente na sua conta" << endl;
+            break;
+        }
+    } while (option != 4);
+    {
+    }
     system("pause>0");
 }
