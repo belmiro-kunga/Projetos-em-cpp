75454c6fe436bdfdfecab9d8c012dad591de0cd8
belmiro-kunga <belmirokunga@gmail.com>
2021-09-09 21:20:39 +0100

    atm

---
+++ ATM application in C++/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void showMenu(){
+    cout << "**********MENU**********"<<endl;
+    cout << "1. verificar saldo"<< endl;
+    cout << "2. deposito"<< endl;
+    cout << "3. levantamento"<< endl;
+    cout << "************************"<<endl;
+}
+
+int main(){
+    //verificar saldo, deposito, levantamento,mostrar menu
+    system("pause>0");
+}
